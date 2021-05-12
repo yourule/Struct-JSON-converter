b093a7302aa9548f604eed7bb2ca583ad126fd5b
Frans-Jan Wind <fjw@yourule.nl>
2021-05-12 22:28:31 +0200

    Initial Commit

---
+++ Struct-JSON-converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Struct-JSON-converter
+//
+//  Created by Frans-Jan Wind on 12/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
